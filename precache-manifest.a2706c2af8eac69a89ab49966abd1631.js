988e6de66492f774e2941c088eb2bd5735684132
Glen McIntosh <glen.e.mcintosh@gmail.com>
2024-06-30 10:36:07 -0600

    Updates

---
+++ precache-manifest.a2706c2af8eac69a89ab49966abd1631.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4dc30f404d9c73d16ca3",
+    "url": "/static/css/main.1d4184fd.chunk.css"
+  },
+  {
+    "revision": "4dc30f404d9c73d16ca3",
+    "url": "/static/js/main.37f99505.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9509b7b1d7f6d1d02d6f",
+    "url": "/static/js/2.44de1018.chunk.js"
+  },
+  {
+    "revision": "7e2b988e72a311d7c26ee6c26c529a4f",
+    "url": "/index.html"
+  }
+];
