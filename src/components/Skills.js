158fc8667bc4549074bde83e62ce9947243a8202
Glen McIntosh <glen.e.mcintosh@gmail.com>
2024-06-30 10:12:12 -0600

    v2

---
+++ src/components/Skills.js
@@ -0,0 +1,76 @@
+import React from "react";
+import { ChipIcon, CodeIcon, CogIcon, BriefcaseIcon } from "@heroicons/react/solid";
+import { skills, skills2, skills3 } from "../data";
+
+export default function Skills() {
+    return (
+        <section id="skills">
+            <div className="container px-5 py-10 mx-auto">
+                <div className="text-center mb-20">
+                <ChipIcon className="w-10 inline-block mb-4" />
+                <h1 className="sm:text-4xl text-3xl font-medium title-font text-gray-800 mb-4">
+                    Skills &amp; Technologies
+                </h1>
+                <p className="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi sit
+                    ipsa delectus eum quo voluptas aspernatur accusantium distinctio
+                    possimus est.
+                </p>
+                </div>
+                <div>
+                    <div className="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2">
+                        <div className="w-full text-center mb-4">
+                            <p className="text-2xl leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto">
+                                Programming
+                            </p>
+                        </div>
+                        {skills.map((skill) => (
+                            <div key={skill} className="p-2 sm:w-1/2 w-full">
+                                <div className="bg-gray-800 rounded flex p-4 h-full items-center">
+                                    <CodeIcon className="text-green-400 w-6 h-6 flex-shrink-0 mr-4" />
+                                    <span className="title-font font-medium text-white">
+                                        {skill}
+                                    </span>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                    <div className="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2">
+                        <div className="w-full text-center mb-4">
+                            <p className="text-2xl leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto">
+                                Technologies
+                            </p>
+                        </div>
+                        {skills2.map((skill) => (
+                            <div key={skill} className="p-2 sm:w-1/2 w-full">
+                                <div className="bg-gray-800 rounded flex p-4 h-full items-center">
+                                    <CogIcon className="text-green-400 w-6 h-6 flex-shrink-0 mr-4" />
+                                    <span className="title-font font-medium text-white">
+                                        {skill}
+                                    </span>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                    <div className="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2">
+                        <div className="w-full text-center mb-4">
+                            <p className="text-2xl leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto">
+                                General
+                            </p>
+                        </div>
+                        {skills3.map((skill) => (
+                            <div key={skill} className="p-2 sm:w-1/2 w-full">
+                                <div className="bg-gray-800 rounded flex p-4 h-full items-center">
+                                    <BriefcaseIcon className="text-green-400 w-6 h-6 flex-shrink-0 mr-4" />
+                                    <span className="title-font font-medium text-white">
+                                        {skill}
+                                    </span>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+}
---
+++ src/data.js
@@ -0,0 +1,89 @@
+export const projects = [
+  {
+    title: "Project Management Web App",
+    subtitle: "Java, React, REST API",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-1.gif",
+    link: "https://reactbootcamp.com",
+  },
+  {
+    title: "Database Management Web App",
+    subtitle: "Java, MySQL, Jakarta",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-2.gif",
+    link: "https://reactbootcamp.com",
+  },
+  {
+    title: "Spotify Song Popularity Predictor",
+    subtitle: "Python, SKLearn, Pandas, Numpy",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-3.gif",
+    link: "https://reactbootcamp.com",
+  },
+  {
+    title: "Spotify Dataset Analytics with Graphical Data Representations ",
+    subtitle: "R, Rplots, Rpart, ggplot2",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-4.gif",
+    link: "https://reactbootcamp.com",
+  },
+];
+
+export const education = [
+  {
+    degree: "Bachelor of Science in Computer Science",
+    college: "Colorado State University",
+    year: 2024,
+  },
+  {
+    degree: "Associate of Applied Science in Automotive Service Technology",
+    college: "Aims Community College",
+    year: 2018,
+  },
+];
+
+export const certifications = [
+  {
+  cert: "Remote Pilot license",
+  id: 4988383,
+  exp: "Expires: 04-2024"
+  }, 
+];
+
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "C/C++",
+  "Java",
+  "Python",
+  "SQL",
+  "LaTex",
+  "R",
+  "REST",
+  "MATLAB",
+];
+
+export const skills2 = [
+  "Windows",
+  "Linux",
+  "Git",
+  "Docker",
+  "CMake",
+  "Microsoft Teams",
+  "Visual Studio",
+  "Intellij",
+];
+
+export const skills3 = [
+  "Teamwork",
+  "Communication",
+  "Organization",
+  "Time Management",
+  "Problem Solving",
+  "Creative thinking",
+];
